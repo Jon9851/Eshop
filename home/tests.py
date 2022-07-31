15e72c48570cefd23e5925b5d498b0649546b108
Jon9851 <joncohen21k@hotmail.co.uk>
2022-07-31 14:37:10 +0000

    basic shopping baf features added still need to add code so itmes can be added to the bag

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    """
+    this test is to check that the index view is rendering
+    the correct template when used
+    """
+
+    def test_get_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
