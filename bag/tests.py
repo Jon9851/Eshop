43512df17dded836a8afaaebdb91c2c6b2c608bb
Jon9851 <joncohen21k@hotmail.co.uk>
2022-08-07 00:47:30 +0000

    chnages to responsiveness on smaller devices on iphone 5

--- bag/tests.py
+++ bag/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestViews(TestCase):
+
+    """
+    this test is to check that the view_bag view is rendering
+    the correct template when used
+    """
+
+    def test_get_view_bag(self):
+        response = self.client.get('/bag/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bag/bag.html')
