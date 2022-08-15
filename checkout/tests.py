a27b049c1392f31206f38de3bfd546f7f7763cd1
Jon9851 <joncohen21k@hotmail.co.uk>
2022-08-15 11:58:09 +0000

    three test ran on views home and bag  passed models test ran and passed on product modles

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,16 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-
-
-class TestViews(TestCase):
-
-    """
-    this test is to check that the checkout view is using 
-    the correct template when used
-    """
-
-    def test_get_view_checkout(self):
-        response = self.client.get('/checkout/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'checkout/checkout.html')
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,15 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-
-
-class TestViews(TestCase):
-
-    """
-    this test is to check that the view_bag view is rendering
-    the correct template when used
-    """
-
-    def test_get_view_profiles(self):
-        response = self.client.get('/profile/')
-        self.assertTemplateUsed(response, 'profiles/profile.html')
